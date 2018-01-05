54b7234e22f12648660fea1fc6f35bd4eb8aed53
MaciejSwiderski <35151062+MaciejSwiderski@users.noreply.github.com>
2018-01-05 23:05:05 +0100

    Add files via upload
    The program  recognizes and  calculate  mathematical operation and writes the log of it straight to the console. The program includes also commented code that allows to write the log straight to the txt file.

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+package org.java2.maciej.swiderski.zadanie009ogolne.zadanie5;
+// Bardzo dobrze. Każda metoda przygotowana osobno przyjmująca argumenty i zwracająca wynik.
+public class Calculator {
+    // Zbędna nowa linia
+
+    public int addition(int a, int b) {
+
+        int sum = a + b;
+        return sum;
+    }
+
+    public int subtraction(int a, int b) {
+
+        int sub = a - b;
+        return sub;
+    }
+
+    public int multiplication(int a, int b) {
+
+        int multi = a * b;
+        return multi;
+    }
+
+    public int division(int a, int b) {
+
+        int div = a / b;
+        return div;
+    }
+
+    public int exponentiation(int a, int b) {
+
+        int toThePowerOf = 1;
+        while (b > 0) {
+            toThePowerOf *= a;
+            b--;
+        }
+        return toThePowerOf;
+    }
+}
